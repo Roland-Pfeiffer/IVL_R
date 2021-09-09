90951454d01bdf921cafd0fbed2ac00c50f4e75c
Roland-Pfeiffer <r.pfeiffer22@web.de>
2021-09-09 16:20:24 +0200

    Works. Using lubridate for datetimes.

--- hobo_logger_data_cleaning.R
+++ hobo_logger_data_cleaning.R
@@ -1,15 +1,24 @@
+# Clear workspace (aka work environment)
 rm(list=ls())
+
+# Close any open plots
 dev.off()
+
+# Clear the console
 cat("\014") 
 
+# Load packages. If you don't have them, install them using:
+# install.packages("package name") # include the quotation marks!
 library(tidyverse)
-library(stringr)  # Check if this also works on WIN. Otherwise use "stringi"
+library(stringr)  # Check if this works on Windows. Otherwise perhaps use "stringi"
 library(tcltk)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(gridExtra)
 
+# -------------------------- FUNCTIONS -----------------------------------------
+# Function defititions who will be used in the script section below.
 
 add_gmt_offset <- function(data_df){
   gmt_offset <- extract_gmt_format(data_df)
@@ -20,11 +29,10 @@ add_gmt_offset <- function(data_df){
 }
 
 
-adjust_datetime <- function(dframe){
+adjust_gmt_offset <- function(dframe){
   dframe$gmt_correction_sec <- (2 - dframe$gmt_offset) * 60 * 60
-  dframe$datetime <- as.POSIXct(dframe$datetime, format="%m/%d/%y %I:%M:%S %p", tz = "Europe/Paris")
   dframe$datetime <- dframe$datetime + dframe$gmt_correction_sec
-  dframe$datetime <- as.POSIXct(dframe$datetime, format="%m/%d/%y %H:%M:%S")
+  dframe <- subset(dframe, select = -c(gmt_correction_sec))
   dframe
 }
 
@@ -55,7 +63,7 @@ extract_depth <- function(fname) {
 
 extract_gmt_format <- function(data_df){
   for (cname in colnames(data_df)){
-    if (startsWith(cname, "Date Time") || startsWith(cname, "datetime")){
+    if (startsWith(cname, "Date Time")){
       dt_match_str <- str_extract(cname, "GMT[+|-]\\d\\d:\\d\\d")
     }}
   dt_match_str
@@ -143,90 +151,94 @@ get_colname <- function(dframe, name_start_str){
 
 
 
-
-
 # -------------------------------- SCRIPT SECTION ------------------------------
 
+
+
 # fpath_browse <- tk_choose.dir("Select folder with HOBO logger .csv files.")
-PATH_TO_FILES <- "/media/findux/DATA/Documents/IVL/Data/HOBO logger data 2019/"
-SAVE_LOCATION <- "/media/findux/DATA/Documents/IVL/Data/hobo_data"
-COND_THRESHOLD <- 2000
+PATH_TO_FILES <- "/media/findux/DATA/Documents/IVL/Data/hobo_data_all/"
+SAVE_LOCATION <- "/media/findux/DATA/Documents/IVL/Data/hobo_out/hobo_data"
+COND_THRESHOLD <- 800
 
 
-DELETE_OUTLIERS <- FALSE
+DELETE_OUTLIERS <- TRUE
 fname_outliers <- "file:///media/findux/DATA/Documents/IVL/Data/outliers.csv"
 
-# Fucktheduck
+# Colors:
+C03 <- "#33CCCC"
+C07 <- "#009999"
+C15 <- "#006666"
+C25 <- "#336666"
 
-# Initiate data frames
-# data_03_m <- data.frame()
-# data_07_m <- data.frame()
-# data_15_m <- data.frame()
-# data_25_m <- data.frame()
+# Initiate empty data frames and lists for later use.
 data_merged <- data.frame()
-skipped_files <- list()
-processed_files <- list()
+skipped_files <- c()
+processed_files <- c()
 
-# Read filenames
+# Read filenames into a list called fnames:
 fnames <- list.files(PATH_TO_FILES)
 
-# Gobble up chaotically named data:
+# Go over every single filename in fnames
 for (fname in fnames){
+  # Check if it ends with ".csv" and then add it to the list of processed files
   if (endsWith(fname, ".csv")){
-    processed_files <- c(processed_files, fname)
+    processed_files[length(processed_files) + 1] <- fname
+    # If filename contains depth information, continue processing it.
     if (fname_contains_depth(fname)){
       depth <- extract_depth(fname)
       depth_num <- depth_as_number(depth)
-      fpath <- paste(PATH_TO_FILES, fname, sep = "/")
+      full_path_to_file <- paste(PATH_TO_FILES, fname, sep = "/")
+      data_tmp <- read.csv(full_path_to_file, skip = 1, check.names = FALSE)
+      # TODO: add an error catch
+      
       message('Depth: ', depth, '\tFile: ', fname)
       
-      # TODO: fix the error treatment
-      possible_read_error <- tryCatch(data_tmp <- read.csv(fpath, skip = 1, check.names = FALSE),
-                                      error = function(e) {skipped_files <<- c(skipped_files, fname)})  # TODO: this does not work yet...
-      if (!inherits(possible_read_error, "error")){  # TODO: this does not escape errors.
-        temperature_units <- get_temp_unit(data_tmp)
-        
-        
-        # Adjust the column names for datetime, temp, 
-        data_tmp <- add_gmt_offset(data_tmp)
-        colnames(data_tmp) <- homogenize_colnames(colnames(data_tmp))
-        
-        if (temperature_units == "F"){
-          message("(Temperature is in Fahrenheit and will be converted to Celsius.)")
-          data_tmp$temp <- convert_f_to_c(data_tmp$temp)
-        }
-        
-        # print(head(data_tmp))
-        data_tmp <- fill_cond_if_missing(data_tmp)
-        data_tmp$conductivity_raw <- as.numeric(data_tmp$conductivity_raw)
-        data_tmp <- select(data_tmp, datetime, temp, conductivity_raw, gmt_offset)
-        data_tmp$depth_m <- depth_num
-        data_tmp$file <- fname
-        
-        # if (depth == "3m"){
-        #   data_03_m <- rbind(data_03_m, data_tmp)
-        # } else if (depth == "7m") {
-        #   data_07_m <- rbind(data_07_m, data_tmp)
-        # } else if (depth == "15m") {
-        #   data_15_m <- rbind(data_15_m, data_tmp)
-        # } else if (depth == "25m") {
-        #   data_25_m <- rbind(data_25_m, data_tmp)
-        # }
-        data_merged <- rbind(data_merged, data_tmp)
-      } else {
-        # Error case
+      # Get temperature units. Needs to be done before colnames are changed.
+      temperature_units <- get_temp_unit(data_tmp)
+      # Get GMT offset. Needs to be done before colnames are changed.
+      data_tmp <- add_gmt_offset(data_tmp)
+      
+      # Adjust the column names for datetime, temp, and (if available) conductivity
+      colnames(data_tmp) <- homogenize_colnames(colnames(data_tmp))
+      
+      # Correct Fahrenheit to Celsius if necessary.
+      if (temperature_units == "F"){
+        message("(Temperature is in Fahrenheit and will be converted to Celsius.)")
+        data_tmp$temp <- convert_f_to_c(data_tmp$temp)
       }
+  
+      data_tmp <- fill_cond_if_missing(data_tmp)
+      data_tmp$conductivity_raw <- as.numeric(data_tmp$conductivity_raw)
+      data_tmp <- select(data_tmp, datetime, temp, conductivity_raw, gmt_offset)
+      data_tmp$depth_m <- depth_num
+      
+      # Add a column w/ filename to see where the data came from.
+      data_tmp$file <- fname
+      
+      # Add currently processed data to the data_merged data frame
+      data_merged <- rbind(data_merged, data_tmp)
     } else {
-      skipped_files <- c(skipped_files, fname)
-    }}}
-rm(list=c("data_tmp",
-          "possible_read_error"))
+      # If filename did not contain depth information, add it to the list of skipped files.
+      skipped_files[length(skipped_files) + 1] <- fname
+    }
+  }
+}
+
+
+# Show info about skipped and processed files
+n_processed <- length(processed_files)
+n_skipped <- length(skipped_files)
+message("Processed ", length(processed_files), " files.\nMerged ", n_processed - n_skipped, " files (", n_skipped, " skipped).")
+if (length(skipped_files) > 0){
+  for (fname in skipped_files){
+    message("Skipped csv file: ", fname)
+  }
+}
+
 # Adjust datetime
 data_merged <- fm_pm_to_AM_FM(data_merged) # Turn fm/em into AM/PM
-data_merged <- adjust_datetime(data_merged)
-
-# Drop columns no longer needed
-data_merged <- subset(data_merged, select = -c(gmt_offset, gmt_correction_sec))
+data_merged$datetime <- mdy_hms(data_merged$datetime)
+data_merged <- adjust_gmt_offset(data_merged)
 
 # Treat depth as factor 
 data_merged$depth_m <- as.factor(data_merged$depth_m)
@@ -239,41 +251,46 @@ len_new <- dim(data_merged)[1]
 message("Removed ", len_old - len_new, " duplicate rows.")
 
 # Plot raw data
-plot_raw <- ggplot(data=data_merged, mapping = aes(x=datetime, y=temp, color=depth_m)) +
-  geom_line() + 
-  ggtitle("Raw data")
-
-# Plot raw
 xlims <- c(min(data_merged$datetime), max(data_merged$datetime))
 ylims <- c(min(data_merged$temp), max(data_merged$temp))
-
+plot_raw <- ggplot(data=data_merged, mapping = aes(x=datetime, y=temp, color=depth_m)) +
+  geom_line() + 
+  ggtitle("Raw data") + 
+  xlim(xlims) 
 plot_03m_raw <- ggplot(data = data_merged[data_merged$depth_m == 3, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#33CCCC" ) +
   ggtitle("03 m raw") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_07m_raw <- ggplot(data = data_merged[data_merged$depth_m == 7, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#009999" ) +
   ggtitle("07 m raw") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_15m_raw <- ggplot(data = data_merged[data_merged$depth_m == 15, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#006666" ) +
   ggtitle("15 m raw") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_25m_raw <- ggplot(data = data_merged[data_merged$depth_m == 25, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#003333" ) +
   ggtitle("25 m raw") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 
-# Save raw data
+
+# Sort the data by datetime
+data_merged <- data_merged[order(data_merged$datetime),]
+
+# Add a numerical timestamp
+data_merged$month <- month(data_merged$datetime)
+data_merged$year <- year(data_merged$datetime)
+data_merged$day <- day(data_merged$datetime)
+data_merged$hour <- hour(data_merged$datetime)
+data_merged$timenum <- data_merged$year * 1000000 + data_merged$month * 10000 + data_merged$day * 100 + data_merged$hour
+
+
 data_raw <- data_merged
-data_raw$datetime <- as.character(data_raw$datetime)
-date_now <- as.character(strptime(now(), format = "%Y-%m-%d"))
-write_csv(data_raw, paste(SAVE_LOCATION, "/hobo_data_raw_", date_now, ".csv", sep = ""))
-
 
 
 # Remove conductivity below threshold rows
@@ -305,15 +322,26 @@ if (DELETE_OUTLIERS){
   }
   data_merged <- data_merged[-c(outlier_indices),]
   d2 <- dim(data_merged)[1]
-  message("Dropped ", d1 - d2, " rows of outliers.")
+  message("Dropped ", d1 - d2, " rows of manually marked outliers.")
 }
 
-# Sort the data by datetime
-data_merged <- data_merged[order(data_merged$datetime),]
 
+# # Aggregate data NOTE: Be patient, this might take a while.
+message("Aggregating data. This might take a while...")
+data_merged <- aggregate(data_merged, by = list(data_merged$datetime, data_merged$depth_m), FUN = "mean")
+# Drop columns with NAs that were created during aggregate()
+data_merged <- subset(data_merged, select = -c(depth_m, datetime, file, id))
+# Update the column names
+names(data_merged)[names(data_merged) == 'Group.1'] <- "datetime"
+names(data_merged)[names(data_merged) == 'Group.2'] <- "depth_m"
+
+# Create a data frame in wide format
 data_wide <- reshape(data_merged, idvar = "datetime", timevar = "depth_m", direction = "wide")
+# Adjust column names
 colnames(data_wide) <- str_replace(colnames(data_wide), "\\.", "_")
-
+for (i  in 2:length(colnames(data_wide))){
+  colnames(data_wide)[i] <- paste(colnames(data_wide)[i], "m", sep = "")
+}
 
 # Plotting 
 plot_all <- ggplot(data = data_merged, mapping = aes(x = datetime, y = temp, color = depth_m)) +
@@ -323,22 +351,22 @@ plot_03m <- ggplot(data = data_merged[data_merged$depth_m == 3, ], mapping = aes
   geom_line(color="#33CCCC" ) +
   ggtitle(label = "03 m") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_07m <- ggplot(data = data_merged[data_merged$depth_m == 7, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#009999" ) +
   ggtitle("07 m") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_15m <- ggplot(data = data_merged[data_merged$depth_m == 15, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#006666" ) +
   ggtitle("15 m") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 plot_25m <- ggplot(data = data_merged[data_merged$depth_m == 25, ], mapping = aes(x = datetime, y = temp)) + 
   geom_line(color="#003333" ) +
   ggtitle("25 m") +
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime)))
+  xlim(xlims) + ylim(ylims)
 
 
 plot_comparison_03 <- ggplot() +
@@ -346,37 +374,25 @@ plot_comparison_03 <- ggplot() +
   geom_line(data = data_merged[data_merged$depth_m == 3, ], mapping = aes(x=datetime, y=temp), color=C07) +
   ggtitle(label = "03 m") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime))) + ylim(plot_ylims)
+  xlim(xlims) + ylim(ylims)
 plot_comparison_07 <- ggplot() +
   geom_line(data = data_raw[data_raw$depth_m == 7, ], mapping = aes(x=datetime, y=temp), color="red") +
   geom_line(data = data_merged[data_merged$depth_m == 7, ], mapping = aes(x=datetime, y=temp), color=C07) +
   ggtitle(label = "07 m") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime))) + ylim(plot_ylims)
+  xlim(xlims) + ylim(ylims)
 plot_comparison_15 <- ggplot() +
   geom_line(data = data_raw[data_raw$depth_m == 15, ], mapping = aes(x=datetime, y=temp), color="red") +
   geom_line(data = data_merged[data_merged$depth_m == 15, ], mapping = aes(x=datetime, y=temp), color=C07) +
   ggtitle(label = "15 m") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime))) + ylim(plot_ylims)
+  xlim(xlims) + ylim(ylims)
 plot_comparison_25 <- ggplot() +
   geom_line(data = data_raw[data_raw$depth_m == 25, ], mapping = aes(x=datetime, y=temp), color="red") +
   geom_line(data = data_merged[data_merged$depth_m == 25, ], mapping = aes(x=datetime, y=temp), color=C07) +
   ggtitle(label = "25 m") + 
   xlab("Time") + ylab("Temp. (°C)") +
-  xlim(c(min(data_merged$datetime), max(data_merged$datetime))) + ylim(plot_ylims)
-
-
-# Show info about processed files and removed duplicates:
-if (length(skipped_files) > 0){
-  for (fname in skipped_files){
-    message("Skipped csv file: ", fname)
-  }
-}
-n_processed <- length(processed_files)
-n_skipped <- length(skipped_files)
-message("Processed ", length(processed_files), " files.\nMerged ", n_processed - n_skipped, " files (", n_skipped, " skipped).")
-message("Removed ", len_old - len_new, " duplicate rows.")
+  xlim(xlims) + ylim(ylims)
 
 
 # Save files
@@ -385,9 +401,13 @@ data_wide$datetime <- as.character(data_wide$datetime)
 date_now <- as.character(strptime(now(), format = "%Y-%m-%d"))
 write_csv(data_merged, paste(SAVE_LOCATION, "_merged_", date_now, ".csv", sep = ""))
 write_csv(data_wide, paste(SAVE_LOCATION, "_wide_", date_now, ".csv", sep = ""))
+data_raw$datetime <- as.character(data_raw$datetime)
+date_now <- as.character(strptime(now(), format = "%Y-%m-%d"))
+write_csv(data_raw, paste(SAVE_LOCATION, "_raw_", date_now, ".csv", sep = ""))
 
-grid.arrange(plot_03m_raw, plot_07m_raw, plot_15m_raw, plot_25m_raw, ncol=1)
-grid.arrange(plot_comparison_03, plot_comparison_07, plot_comparison_15, plot_comparison_25, ncol=1)
+
+# grid.arrange(plot_03m_raw, plot_07m_raw, plot_15m_raw, plot_25m_raw, ncol=1)
+# grid.arrange(plot_comparison_03, plot_comparison_07, plot_comparison_15, plot_comparison_25, ncol=1)
 # plot_15m
 # plot_raw
 # plot_all
--- tests_fmem_to_AMPM.R
+++ tests_fmem_to_AMPM.R
@@ -1,13 +1,14 @@
 library(stringr)
+library(lubridate)
+
 
 fm_pm_to_AM_FM <- function(df){
-  
   df$datetime <- str_replace(df$datetime, "em", "PM")
   df$datetime <- str_replace(df$datetime, "fm", "AM")
   return(df)
 }
 
-fname <- "file:///media/findux/DATA/Documents/IVL/Data/HOBO logger data 2019/20200330_Logger7m.csv"
+fname <- "file:///media/findux/DATA/Documents/IVL/Data/hobo_data_all/20200330_Logger7m.csv"
 data <- read.csv(fname, check.names = FALSE, skip = 1)
 data <- data[, 2:4]
 colnames(data) <- c("datetime", "cond", "temp")
@@ -16,3 +17,6 @@ data$datetime <- str_replace(data$datetime, "em", "PM")
 data$datetime <- str_replace(data$datetime, "fm", "AM")
 
 data <- fm_pm_to_AM_FM(data)
+
+data$datetime <- mdy_hms(data$datetime)
+data$datetime <- data$datetime + 20
